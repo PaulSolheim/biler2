546922398736e34a33d0f46a830670a71e8788be
Pål Grønstad Solheim <paul.solheim@gmail.com>
2019-01-11 02:14:51 +0100

    Klart til oppstart
    Oppstart utfra hvor langt gruppe 3 kom den første kodekvelden.

---
+++ js/scenes/sceneMain.js
@@ -0,0 +1,26 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload() {
+    }
+
+    create() {
+         //set up
+        emitter=new Phaser.Events.EventEmitter();
+        controller=new Controller();
+        mediaManager=new MediaManager({scene:this});
+        var sb=new SoundButtons({scene:this});
+        //
+        //define our objects
+        this.road = new Road({
+            scene: this
+        });
+        this.road.x = game.config.width * .5;
+        
+    }
+
+    update() {
+        //constant running loop
+    }
+}
